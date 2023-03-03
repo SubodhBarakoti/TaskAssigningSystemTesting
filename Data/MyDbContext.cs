8ae0fe7847122d75ad2d9a7f3d543f600f57b331
SubodhBarakoti <subodhbarakoti17@gamil.com>
2023-03-03 20:22:14 +0545

    Added a new DBContext File

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskAssigningSystem.Models;
+
+namespace TaskAssigningSystem.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+
+    }
+}
