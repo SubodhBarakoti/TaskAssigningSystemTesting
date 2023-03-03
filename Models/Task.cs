be08a4952009daa2d6fb6514ce94e3c3e4847b4c
Pratik2254 <shresthapratik660@gmail.com>
2023-03-03 21:24:30 +0545

    TAsk class

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,6 +1,31 @@
-﻿namespace TaskAssigningSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace TaskAssigningSystem.Models
 {
     public class Task
     {
+        [Key]
+        public Guid TaskId { get; set; }
+        [Required]
+        [DisplayName("Task Name")]
+        [MaxLength(30, ErrorMessage = "Name of skill should have at most 30 characters.")]
+        public string? TaskName{ get; set; }
+
+        [Required]
+        [DisplayName("Task Description")]
+        public string? TaskDescription { get; set; } 
+
+        [Required]
+        [DisplayName("Type of Task")]
+        public string? TaskType { get; set; }
+
+        [Required]
+        [DisplayName("Task Status")]
+        public int TaskStatus { get; set; }
+
+
+
+
+
     }
 }
