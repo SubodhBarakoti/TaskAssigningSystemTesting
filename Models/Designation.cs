a02da2ff78f18dd1d9608f237f653de8032aad67
Pratik2254 <shresthapratik660@gmail.com>
2023-03-03 21:09:00 +0545

    designation class

---
+++ Models/Designation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskAssigningSystem.Models
+{
+    public class Designation
+    {
+        [Key]
+        public Guid DesignationId { get; set; }
+        
+
+        [Required]
+        [MaxLength(30,ErrorMessage ="Designation Name must be atmost 30 character.")]
+        public string? DesignationName { get; set; }
+
+
+    }
+}
